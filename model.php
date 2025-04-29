85ace1b55b5a977759600106084ba38f397f61cd
T1INGs <133334646+T1INGs@users.noreply.github.com>
2025-04-29 09:45:48 +0700

    Update model.php

--- model.php
+++ model.php
@@ -3,9 +3,9 @@
 namespace App\Models;
 use CodeIgniter\Model;
 
-class BukuModel extends Model
+class BukukuModel extends Model
 {
-    protected $table = 'buku';
+    protected $table = 'bukuku';
     protected $primaryKey = 'id';
     protected $allowedFields = ['judul', 'pengarang', 'penerbit', 'tahun_terbit', 'isbn'];
 }
